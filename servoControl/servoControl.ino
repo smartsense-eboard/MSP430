ebe263b6afb4b4dce969a6691f381fa1f3c8ca1a
Dylan Kite <dylankite@borealiswind.com>
2019-07-22 12:09:08 -0400

    end of day

--- servoControl/servoControl.ino
+++ servoControl/servoControl.ino
@@ -1,55 +1,67 @@
+//Libraries Needed
 #include <Servo.h> 
 #include <Wire.h>
 
-#define LED RED_LED
 
 
-const int ledPin = 13; // onboard LED
-Servo motors;    // create servo object to control a servo 
-int angle = 0;    // variable to store the servo position 
 
+Servo motors;    // create servo object to control a servo
+unsigned long micro = 1200;    // variable to store the servo position
+int data[3];
+unsigned long val = 1000;
+unsigned long now = 1000;
 
 void setupServo(void)
 {
-  Wire.begin(0x8);                // join i2c bus with address #8
+  Wire.begin(0x8);                // join I2C bus with address #8
   Wire.onReceive(receiveEvent); // register event
-  pinMode(LED, OUTPUT);
-  digitalWrite(ledPin, LOW); // turn it off
   Serial.begin(9600);
 }
 
 
 void setup() 
-{ 
+{
   motors.attach(4);  // attaches the servo on pin 4 to the servo object 
   setupServo();
 }
  
-void loop() 
+void loop()
 {
-motors.write(angle);
-//delay(1); //delay ms may or may not need 
-  
+ if(val > now)
+ {
+  now = now + 20;
+ }
+ else if(val < now)
+ {
+  now = now - 20;
+  delay(10);
+  if((now < 1500) and (val < 1400))
+  {
+    now = 1000;
+  }
+ }
+ motors.writeMicroseconds(now);
+ delay(100);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany) {
-  while (Wire.available()) { // loop through all but the last
-    int c = Wire.read(); // receive byte as a character
-    Serial.println(c);
-    angle = c;
+
+void receiveEvent(int howMany)
+{
+  while (Wire.available())
+  { // loop through all but the last
+      int i = 0;
+
+      for(i=0;i< howMany;i++)
+      {
+        data[i] = Wire.read();
+      }
+
+      micro = data[2];
+      micro = micro << 8;
+      micro = micro + data[1];
+      val = micro;
+      Serial.println(val);
   }
 }
-
-/*  for(angle = 0; angle< 180; angle++)  // goes from 0 degrees to 180 degrees 
-  {                                       // in steps of 1 degree 
-    //motors.write(angle);              // tell servo to go to position in variable 'angle’ 
-    delay(20);                       // waits 20ms for the servo to reach the position 
-  } 
-  for(angle = 180;angle>=1; angle--)     // goes from 180 degrees to 0 degrees 
-  {                                
-   // motors.write(angle);              // tell servo to go to position in variable 'angle' 
-    delay(20);                       // waits 20ms for the servo to reach the position 
-  } 
-}*/
